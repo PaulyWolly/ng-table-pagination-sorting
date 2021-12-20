2ed1b138bdeedd321ad3f5fb98a5582562a94a8d
Paul Welby <pwelby@gmail.com>
2021-12-19 22:30:51 -0800

    updated environment var to point to production for running in Netlify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MaterialExampleModule} from '../material.module';
+
 import {TablePaginationExample} from './table-pagination-example';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatNativeDateModule} from '@angular/material/core';
 import {HttpClientModule} from '@angular/common/http';
 import {AngularFirestoreModule} from '@angular/fire/compat/firestore'
 import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBq1_ca-FVW0HEBj8p1ArfyXr2TsF8ngV8",
+    authDomain: "user-portal-1960.firebaseapp.com",
+    projectId: "user-portal-1960",
+    storageBucket: "user-portal-1960.appspot.com",
+    messagingSenderId: "66757668208",
+    appId: "1:66757668208:web:37e8fb731de4c72e5733b5"
+  }
 };
